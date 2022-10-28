a4989d2dbda266cc3914e86110667831ef7c15f9
Lobova Marina <9119971642a@gmail.com>
2022-10-28 22:41:19 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] array = {4, 10, 6, 2, 13, 12, 30};
+        checkArray(array);
+
+        var name = in.next();
+        greetingForVyacheslav(name);
+
+        int i = in.nextInt();
+        hello(i);
+    }
+
+    public static void hello(int number) {
+        if (number > 7) {
+            System.out.println("Hello");
+        }
+    }
+
+    public static void greetingForVyacheslav(String name) {
+        var checkName = "Вячеслав";
+        if (name.equals(checkName)) {
+            System.out.println("Привет, Вячеслав");
+        } else {
+            System.out.println("Нет такого имени");
+        }
+    }
+
+
+    public static void checkArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+
+}
